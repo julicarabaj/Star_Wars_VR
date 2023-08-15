114bc4a09796678c1da7682af547491fd83fdc23
CARABAJAL JULIETA <127246404+julicarabaj@users.noreply.github.com>
2023-08-15 12:08:48 -0300

    Enemigo con arma

---
+++ Assets/Scripts/Lasers.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lasers : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed;
+    }
+
+    void OnCollisionEnter()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TurnToLookAt.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnToLookAt : MonoBehaviour
+{
+    public Transform targetTR;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentEulerAngles = transform.eulerAngles;
+        transform.LookAt(targetTR);
+        transform.eulerAngles = new Vector3(currentEulerAngles.x, transform.eulerAngles.y, currentEulerAngles.z);
+    }
+}
